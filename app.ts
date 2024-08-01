d36a10edac285cf5ccab6a09fa5e05a6260ff5e7
Abhinil Nath <abhinilnath10@gmail.com>
2024-08-02 02:03:38 +0530

    Added payment gateway for membership

---
+++ app.ts
@@ -0,0 +1,31 @@
+require("dotenv").config();
+import express, { Application } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import paymentRouter from "./routes/payment.routes";
+
+const app: Application = express();
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: ["http://localhost:3000"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors());
+app.use("/api/payment", paymentRouter);
+app.get("/", (req, res) => {
+  res.status(200).send("Health check endpoint for server");
+});
+
+app.get("/api/v1/getKey", (req, res) => {
+  res.status(200).json({
+    key_id: process.env.RAZORPAY_KEY_ID,
+  });
+});
+
+export { app };
---
+++ controllers/payment.controller.ts
@@ -0,0 +1,58 @@
+import { instance } from "../server";
+import { Request, Response } from "express";
+import crypto from "crypto";
+import { Payment } from "../models/payment.model";
+export const checkout = async (req: Request, res: Response) => {
+  const { amount } = req.body;
+  const options = {
+    amount: Number(amount * 100),
+    currency: "INR",
+  };
+  const order = await instance.orders.create(options);
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+};
+
+export const paymentVerification = async (req: Request, res: Response) => {
+  const {
+    razorpay_order_id,
+    razorpay_payment_id,
+    razorpay_signature,
+    studentID,
+  } = req.body;
+
+  const body = razorpay_order_id + "|" + razorpay_payment_id;
+
+  const expectedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET as string)
+    .update(body.toString())
+    .digest("hex");
+
+  const isAuthentic = expectedSignature === razorpay_signature;
+  console.log(isAuthentic);
+  if (isAuthentic) {
+    // Database comes here
+
+    await Payment.create({
+      razorpay_order_id,
+      razorpay_payment_id,
+      razorpay_signature,
+      studentID,
+    });
+
+    //TODO: Add the studentID to the student database
+
+    return res.status(200).json({
+      success: true,
+      razorpay_order_id,
+    });
+    // res.redirect(`http://localhost:3000`);
+  } else {
+    res.status(400).json({
+      success: false,
+    });
+  }
+};
